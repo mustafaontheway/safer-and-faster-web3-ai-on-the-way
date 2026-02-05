f97c04cb35abc6bef49ee99b2df3ed832d490764
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-02-05 20:56:06 +0300

    Create part-y-0071-loop1.rs

---
+++ part-y-0071-loop1.rs
@@ -0,0 +1,106 @@
+fn main() {
+
+    let mut seconds: u8 = 0;
+
+    loop {
+        seconds += 1;
+
+        println!("We need a minute. Seconds: {} and minute: {}", seconds, (seconds as f32 / 60.0));
+
+        if seconds == 60 {
+
+            break
+        }
+    }
+}
+
+"""
+We need a minute. Seconds: 1 and minute: 0.016666668
+We need a minute. Seconds: 2 and minute: 0.033333335
+We need a minute. Seconds: 3 and minute: 0.05       
+We need a minute. Seconds: 4 and minute: 0.06666667 
+We need a minute. Seconds: 5 and minute: 0.083333336
+We need a minute. Seconds: 6 and minute: 0.1        
+We need a minute. Seconds: 7 and minute: 0.11666667 
+We need a minute. Seconds: 8 and minute: 0.13333334 
+We need a minute. Seconds: 9 and minute: 0.15       
+We need a minute. Seconds: 10 and minute: 0.16666667
+We need a minute. Seconds: 11 and minute: 0.18333334
+We need a minute. Seconds: 12 and minute: 0.2       
+We need a minute. Seconds: 13 and minute: 0.21666667
+We need a minute. Seconds: 14 and minute: 0.23333333
+We need a minute. Seconds: 15 and minute: 0.25
+We need a minute. Seconds: 16 and minute: 0.26666668
+We need a minute. Seconds: 17 and minute: 0.28333333
+We need a minute. Seconds: 18 and minute: 0.3
+We need a minute. Seconds: 19 and minute: 0.31666666
+We need a minute. Seconds: 20 and minute: 0.33333334
+We need a minute. Seconds: 21 and minute: 0.35
+We need a minute. Seconds: 22 and minute: 0.36666667
+We need a minute. Seconds: 23 and minute: 0.38333333
+We need a minute. Seconds: 24 and minute: 0.4
+We need a minute. Seconds: 25 and minute: 0.41666666
+We need a minute. Seconds: 26 and minute: 0.43333334
+We need a minute. Seconds: 27 and minute: 0.45
+We need a minute. Seconds: 28 and minute: 0.46666667
+We need a minute. Seconds: 29 and minute: 0.48333332
+We need a minute. Seconds: 30 and minute: 0.5
+We need a minute. Seconds: 31 and minute: 0.51666665
+We need a minute. Seconds: 32 and minute: 0.53333336
+We need a minute. Seconds: 33 and minute: 0.55
+We need a minute. Seconds: 34 and minute: 0.56666666
+We need a minute. Seconds: 35 and minute: 0.5833333
+We need a minute. Seconds: 36 and minute: 0.6
+We need a minute. Seconds: 37 and minute: 0.6166667
+We need a minute. Seconds: 38 and minute: 0.6333333
+We need a minute. Seconds: 39 and minute: 0.65
+We need a minute. Seconds: 40 and minute: 0.6666667
+We need a minute. Seconds: 41 and minute: 0.68333334
+We need a minute. Seconds: 42 and minute: 0.7
+We need a minute. Seconds: 40 and minute: 0.6666667
+We need a minute. Seconds: 41 and minute: 0.68333334
+We need a minute. Seconds: 42 and minute: 0.7
+We need a minute. Seconds: 41 and minute: 0.68333334
+We need a minute. Seconds: 42 and minute: 0.7
+We need a minute. Seconds: 43 and minute: 0.71666664
+We need a minute. Seconds: 44 and minute: 0.73333335
+We need a minute. Seconds: 45 and minute: 0.75
+We need a minute. Seconds: 46 and minute: 0.76666665
+We need a minute. Seconds: 47 and minute: 0.78333336
+We need a minute. Seconds: 48 and minute: 0.8
+We need a minute. Seconds: 49 and minute: 0.81666666
+We need a minute. Seconds: 50 and minute: 0.8333333
+We need a minute. Seconds: 45 and minute: 0.75
+We need a minute. Seconds: 46 and minute: 0.76666665
+We need a minute. Seconds: 47 and minute: 0.78333336
+We need a minute. Seconds: 48 and minute: 0.8
+We need a minute. Seconds: 49 and minute: 0.81666666
+We need a minute. Seconds: 50 and minute: 0.8333333
+We need a minute. Seconds: 46 and minute: 0.76666665
+We need a minute. Seconds: 47 and minute: 0.78333336
+We need a minute. Seconds: 48 and minute: 0.8
+We need a minute. Seconds: 49 and minute: 0.81666666
+We need a minute. Seconds: 50 and minute: 0.8333333
+We need a minute. Seconds: 47 and minute: 0.78333336
+We need a minute. Seconds: 48 and minute: 0.8
+We need a minute. Seconds: 49 and minute: 0.81666666
+We need a minute. Seconds: 50 and minute: 0.8333333
+We need a minute. Seconds: 48 and minute: 0.8
+We need a minute. Seconds: 49 and minute: 0.81666666
+We need a minute. Seconds: 50 and minute: 0.8333333
+We need a minute. Seconds: 50 and minute: 0.8333333
+We need a minute. Seconds: 51 and minute: 0.85
+We need a minute. Seconds: 51 and minute: 0.85
+We need a minute. Seconds: 52 and minute: 0.8666667
+We need a minute. Seconds: 53 and minute: 0.8833333
+We need a minute. Seconds: 52 and minute: 0.8666667
+We need a minute. Seconds: 53 and minute: 0.8833333
+We need a minute. Seconds: 54 and minute: 0.9
+We need a minute. Seconds: 55 and minute: 0.9166667
+We need a minute. Seconds: 56 and minute: 0.93333334
+We need a minute. Seconds: 57 and minute: 0.95
+We need a minute. Seconds: 58 and minute: 0.96666664
+We need a minute. Seconds: 59 and minute: 0.98333335
+We need a minute. Seconds: 60 and minute: 1
+
+"""
