9b4c0824e304a8bcc44e79bcaed198c227ed312d
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-02-05 20:09:17 +0300

    Create part-y-0070-match3.rs

---
+++ part-y-0070-match3.rs
@@ -0,0 +1,17 @@
+fn main() {
+
+    let year: u16 = 2026;
+
+    match year {
+
+        2025 => println!("Last year: {year}"),
+
+        2026 => println!("Current year: {year}"),
+
+        2027 => println!("Next year: {year}"),
+
+        _ => println!("?")
+        
+    }
+
+}
